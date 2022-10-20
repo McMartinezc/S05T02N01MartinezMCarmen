89ac445c43d4cf43289434edbaf3d109f63f48b5
Mc <psyence_mc@hotmail.com>
2022-10-20 12:51:11 +0200

    Initial commit

---
+++ src/main/java/com/jocDeDaus/MartinezMCarmen/S05T02N01MartinezMCarmen/S05T02N01MartinezMCarmenApplication.java
@@ -0,0 +1,13 @@
+package com.jocDeDaus.MartinezMCarmen.S05T02N01MartinezMCarmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T02N01MartinezMCarmenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T02N01MartinezMCarmenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jocDeDaus/MartinezMCarmen/S05T02N01MartinezMCarmen/S05T02N01MartinezMCarmenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jocDeDaus.MartinezMCarmen.S05T02N01MartinezMCarmen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T02N01MartinezMCarmenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
