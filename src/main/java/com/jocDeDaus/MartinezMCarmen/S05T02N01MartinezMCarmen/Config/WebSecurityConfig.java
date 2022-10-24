e7401640e81beae11d001cf746035bc2ebebf756
Mc <psyence_mc@hotmail.com>
2022-10-24 12:53:54 +0200

    "v1"

--- src/main/java/com/jocDeDaus/MartinezMCarmen/S05T02N01MartinezMCarmen/Config/WebSecurityConfig.java
+++ src/main/java/com/jocDeDaus/MartinezMCarmen/S05T02N01MartinezMCarmen/Config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.jocDeDaus.MartinezMCarmen.S05T02N01MartinezMCarmen.Config;
 
 import org.springframework.context.annotation.Configuration;
+//https://www.bezkoder.com/spring-boot-jwt-authentication/
+//https://github.com/FrankLuo666/spring-boot-login-example-with-Spring-Security-MySQL-and-JWT/blob/master/README.md
 
-@EnableWebSecurity
 @Configuration
-
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 }
